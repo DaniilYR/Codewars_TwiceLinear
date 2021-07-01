1e92d4293449cdd46afc60c20ef05a1121fc3ba4
DaniilYR <daniil8303@gmail.com>
2021-07-01 22:26:02 +0300

    ...

---
+++ src/TwiceLinear.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <set>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int dblLinear(int n){
+	std::set<int> u;
+	u.insert(1);
+	std::set<int>::iterator it = u.begin();
+	while(u.size() < 2*n){
+		u.insert(2*(*it)+1);
+		u.insert(3*(*it)+1);
+		it++;
+	}
+	
+	it = u.begin();
+	int i = 0;
+	while (i != n)
+	{
+		i++;
+		it++;
+	}
+	return *it;
+}
+
+int main(){
+	//int n = 10;
+	std::cout << dblLinear(10) << std::endl;
+	std::cout << dblLinear(20) << std::endl;
+	std::cout << dblLinear(30) << std::endl;
+	std::cout << dblLinear(50) << std::endl;
+	/*auto print = [](const int& n) { std::cout << " " << n; };
+	auto ans = dblLinear(n);
+	std::for_each(ans.begin(), ans.end(), print);
+	std::cout << std::endl;*/
+	return 0;
+}
